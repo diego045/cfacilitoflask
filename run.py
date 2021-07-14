1fdf181336be8d594c9cab620d83953660a66479
diego <diegonava@ciencias.unam.mx>
2021-07-14 16:54:05 -0500

    coment templates

--- run.py
+++ run.py
@@ -38,6 +38,8 @@ def params():
 def validarruta(columna = 'valor por default', id = ''):
     return f'el parametro es {columna}, {id}'
 
+#Un template o plantilla es una herramienta muy útil para separar el diseño web de la programación de las funcionalidades de la página web.
+#El poder de usas templates en un desarrollo web tiene la ventaja de separar las funciones: diseño y programación
 @app.route('/template')
 def index():
     return render_template('index.html')
